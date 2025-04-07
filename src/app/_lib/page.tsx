8622a1db4f773b7208cd0a421c0f236f5999cf68
shihabhq <adnansaim15@yahoo.com>
2025-04-07 09:34:48 +0600

    private route added

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,10 @@
+const Library = () => {
+  return (
+    <div>
+      This component will not be shown as it is inside _lib folder cuz _ is
+      given before the folder name which means it is a private folder.
+    </div>
+  );
+};
+
+export default Library;
