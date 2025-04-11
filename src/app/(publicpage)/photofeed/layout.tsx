ab074a4f45b5915f29dae9b2362e0b8649e37613
shihabhq <adnansaim15@yahoo.com>
2025-04-11 19:17:28 +0600

    sample photofeed project made using intercepting routes and parallel routes

---
+++ src/app/(publicpage)/photofeed/layout.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Layout(props: {
+  modal: React.ReactNode;
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {props.modal}
+      {props.children}
+    </>
+  );
+}
